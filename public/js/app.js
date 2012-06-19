b8dbc98fd3cf3754802f43d0c9d97110935cdf01
Peter West <pete@whiteoctober.co.uk>
2012-06-19 16:58:38 +0100

    Built script loader for ajax page load

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,25 @@
 $(function() {
 
+  // Set up stylish select boxes
+  var initialisePage = function(page) {
+    page.find("select").chosen({
+      disable_search_threshold: 5,
+      no_results_text: "No formats called",
+      allow_single_deselect: true
+    });
+  }
+
+  // Run any initialisation on page load
+  initialisePage($("body"));
+
   // Sets up push state history for AJAX links
   $('.xhr').live('click', function() {
     var self = $(this);
     if (self.is("a")) var url = self.attr('href');
     if (self.is("input[type=submit]")) var url = "?"+self.closest("form").serialize();
+    var state = window.History.getState();
+    console.log(url);
+    console.log(state);
     window.History.pushState(self.data('history'), "", url);
     return false;
   });
@@ -21,6 +36,7 @@ $(function() {
     url = url.replace("?&", "?");
 
     $('#content').load(url, function(response, status) {
+      initialisePage($(this));
       if(window._gaq) window._gaq.push(['_trackPageview']);
     });
   });
