22b37ac02262f2daed7b4f4d8f24717fd9a767b3
Peter West <pete@whiteoctober.co.uk>
2012-06-29 21:38:03 +0100

    Updated code to automatically update format/checkboxes, added jquery form, simplified styles and added search form partial

--- public/js/app.js
+++ public/js/app.js
@@ -13,16 +13,17 @@ $(function() {
   initialisePage($("body"));
 
   // Sets up push state history for AJAX links
-  $('.xhr').live('click', function() {
+  var changeState = function() {
     var self = $(this);
     if (self.is("a")) var url = self.attr('href');
-    if (self.is("input[type=submit]")) var url = "?"+self.closest("form").serialize();
+    if (self.is("input") || self.is("select")) var url = "?"+self.closest("form").serialize();
     var state = window.History.getState();
-    console.log(url);
-    console.log(state);
     window.History.pushState(self.data('history'), "", url);
     return false;
-  });
+  }
+
+  $('.xhr-click').live('click', changeState);
+  $('.xhr-change').live('change', changeState);
 
   //Monitor state changes for Back request
   window.History.Adapter.bind(window, 'statechange', function() {
@@ -30,7 +31,7 @@ $(function() {
     var url = state.url;
 
     //Add the xhr param (allows for caching + doesn't clash with 'full' pages)
-    url += (url.match(/\\\?/) ? '?' : '&') + 'xhr=true';
+    url += (url.match(/\?/) ? '&' : '?') + 'xhr=true';
 
     //jQuery Cache bug fix
     url = url.replace("?&", "?");
